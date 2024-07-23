11226a01d6e3271847bf8c3eb276bf32f3d85269
renku33 <kurosakirenku@gmail.com>
2024-07-23 09:57:16 +0200

    TESTED FEATURE:  Create the 4 x 3 board

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,21 @@
 export default function Home () {
   return (
+    <>
+      <h1 data-testid='app-title'>Hello React!</h1>
+      <div data-testid='memoryCard' />
+      <div data-testid='memoryCard' />
+      <div data-testid='memoryCard' />
+      <div data-testid='memoryCard' />
 
-    <h1 data-testid='app-title'>Hello React!</h1>
+      <div data-testid='memoryCard' />
+      <div data-testid='memoryCard' />
+      <div data-testid='memoryCard' />
+      <div data-testid='memoryCard' />
 
+      <div data-testid='memoryCard' />
+      <div data-testid='memoryCard' />
+      <div data-testid='memoryCard' />
+      <div data-testid='memoryCard' />
+    </>
   )
 }
